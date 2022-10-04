969cf651c602c4fec02d9689600ba25519137782
Roland Gulya <mgulyaroland@gmail.com>
2022-10-04 08:40:44 +0200

    message Class Created

---
+++ WpfApp1/Clases/Message.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Clases
+{
+    public class Message
+    {
+		private string content;
+		private DateTime date;
+		private string senderName;
+		public Message()
+		{
+
+		}
+
+		public Message(string content, DateTime date, string senderName):this()
+		{
+			this.content = content;
+			this.date = date;
+			this.senderName = senderName;
+		}
+
+		public string Content
+		{
+			get { return content; }
+			set { content = value; }
+		}
+
+		public DateTime Date
+		{
+			get { return date; }
+			set { date = value; }
+		}
+
+		public string  SenderName
+		{
+			get { return senderName; }
+			set { senderName = value; }
+		}
+
+		public override string ToString()
+		{
+			return $"{this.senderName} külde {this.date}-kor";
+		}
+	}
+}
